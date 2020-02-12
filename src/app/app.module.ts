9c56452bdea91531dc027186287fd78905b64f42
zonecc <bkevery@gmail.com>
2020-02-12 17:35:15 +0300

    finish setup and component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostsComponent } from './posts/posts.component';
+import { ShowComponent } from './posts/show/show.component';
+import { EditComponent } from './posts/edit/edit.component';
+import { AddComponent } from './posts/add/add.component';
+import { DeleteComponent } from './posts/delete/delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PostsComponent,
+    ShowComponent,
+    EditComponent,
+    AddComponent,
+    DeleteComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
