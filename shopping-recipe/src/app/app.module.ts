6a41ac3c72de6cdb4579a7874d0cb5d66ad15a67
zonecc <bkevery@gmail.com>
2020-02-15 14:18:06 +0300

    main project setup

---
+++ shopping-recipe/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/edit.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { RecipeBookItemComponent } from './components/recipe/recipe-list/recipe-item/item.component';
+import { RecipeBookListComponent } from './components/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './components/recipe/recipe-detail/recipe-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeComponent,
+    RecipeBookItemComponent,
+    RecipeBookListComponent,
+    RecipeDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
